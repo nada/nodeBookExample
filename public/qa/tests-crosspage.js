d6881e62ac3541ec78de21bd2bb141ddad7351c5
nada <ursbreton@gmail.com>
2014-08-27 23:13:22 +0200

    yat

--- public/qa/tests-crosspage.js
+++ public/qa/tests-crosspage.js
@@ -8,7 +8,7 @@ suite('Cross-Page Tests', function(){
 
 	test('requesting a group rate quote from river bla' +
 		'should populate the referrer field', function(done){ 
-			var referrer = 'http://localhost:3000/tours/hood-river'; 
+			var referrer = 'http://127.0.0.1:3000/tours/hood-river'; 
 			browser.visit(referrer, function(){
 				browser.clickLink('.requestGroupRate', 
 					function(){ 
@@ -21,7 +21,7 @@ suite('Cross-Page Tests', function(){
 	test('requesting a group rate from the oregon coast tour page should ' + 
 		'populate the referrer field', function(done){
 
-		var referrer = 'http://localhost:3000/tours/oregon-coast'; 
+		var referrer = 'http://127.0.0.1:3000/tours/oregon-coast'; 
 		browser.visit(referrer, function(){
 			browser.clickLink('.requestGroupRate', 
 				function(){ assert(browser.field('referrer').value === referrer);
@@ -32,7 +32,7 @@ suite('Cross-Page Tests', function(){
 
 	test('visiting the "request group rate" page dirctly should result ' + 
 		'in an empty referrer field', function(done){
-				browser.visit('http://localhost:3000/tours/request-group-rate', 
+				browser.visit('http://127.0.0.1:3000/tours/request-group-rate', 
 					function(){
                        assert(browser.field('referrer').value === '');
                        done();
